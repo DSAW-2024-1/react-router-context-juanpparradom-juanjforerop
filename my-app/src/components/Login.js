f835358e8280587f7f8a50fe9950f8b1a3710f65
juanfope <157375092+juanfope@users.noreply.github.com>
2024-04-28 17:25:35 -0500

    localStorageUser+LoginWindowFix

--- my-app/src/components/Login.js
+++ my-app/src/components/Login.js
@@ -1,10 +1,38 @@
+import React, {Fragment, useState} from "react"
+import ReactDOM from "react-dom"
 export default function Login(){
+    const [text, setText] = useState(
+        window.localStorage.getItem('text')
+    )
+
+    const setLocalStorage = value => {
+        try {
+            setText(value)
+            window.localStorage.setItem("text", value)
+        } catch(error) {
+            console.error(error)
+        }
+    }
     return(
         <div>
-            <h3>Ingrese su correo:</h3>
-            <input>example123@gmail.com</input>
-            <h3>Ingrese su contraseña:</h3>
-            <input>password123</input>
+            <h3>Ingrese su correo electronico</h3>
+            <form>
+                <input 
+                type="text" 
+                onChange={e => setLocalStorage(e.target.value)}
+                value={text}/>
+            </form>
+            <h3>Ingrese su contraseña</h3>
+            <form>
+                <input 
+                type="text" 
+                />
+            </form>
         </div>
     )
 }
+
+/*<h3>Ingrese su correo:</h3>
+            <input>example123@gmail.com</input>
+            <h3>Ingrese su contraseña:</h3>
+            <input>password123</input>*/
